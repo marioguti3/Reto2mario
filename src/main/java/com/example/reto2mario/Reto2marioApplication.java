68b8dfac9d5b7e8af7ca47a3a17567cacc429d27
Mario <gutierrezmontoriomario@gmail.com>
2021-02-26 16:19:01 +0100

    first commit

---
+++ src/main/java/com/example/reto2mario/Reto2marioApplication.java
@@ -0,0 +1,13 @@
+package com.example.reto2mario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2marioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2marioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reto2mario/Reto2marioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reto2mario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto2marioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
